cec686437b0a2f7ef085a2c397fe03230b82876f
cgiguere <c.giguere42@gmail.com>
2021-03-08 14:13:20 -0500

    remove webworkify dep

--- src/util/workerSocketImpl.js
+++ src/util/workerSocketImpl.js
@@ -1,48 +1,46 @@
-var WebSocket = WebSocket || require('ws');
-
-module.exports = function(self) {
-  var socket = null;
+module.exports = function (self) {
+  var socket = null
 
   function handleSocketMessage(ev) {
-    var data = ev.data;
+    var data = ev.data
 
     if (data instanceof ArrayBuffer) {
       // binary message, transfer for speed
-      self.postMessage(data, [data]);
+      self.postMessage(data, [data])
     } else {
       // JSON message, copy string
-      self.postMessage(data);
+      self.postMessage(data)
     }
   }
 
   function handleSocketControl(ev) {
-    self.postMessage({type: ev.type});
+    self.postMessage({ type: ev.type })
   }
 
-  self.addEventListener('message', function(ev) {
-    var data = ev.data;
+  self.addEventListener('message', function (ev) {
+    var data = ev.data
 
     if (typeof data === 'string') {
       // JSON message from ROSLIB
-      socket.send(data);
+      socket.send(data)
     } else {
       // control message
       if (data.hasOwnProperty('close')) {
-        socket.close();
-        socket = null;
+        socket.close()
+        socket = null
       } else if (data.hasOwnProperty('uri')) {
-        var uri = data.uri;
+        var uri = data.uri
 
-        socket = new WebSocket(uri);
-        socket.binaryType = 'arraybuffer';
+        socket = new WebSocket(uri)
+        socket.binaryType = 'arraybuffer'
 
-        socket.onmessage = handleSocketMessage;
-        socket.onclose = handleSocketControl;
-        socket.onopen = handleSocketControl;
-        socket.onerror = handleSocketControl;
+        socket.onmessage = handleSocketMessage
+        socket.onclose = handleSocketControl
+        socket.onopen = handleSocketControl
+        socket.onerror = handleSocketControl
       } else {
-        throw 'Unknown message to WorkerSocket';
+        throw 'Unknown message to WorkerSocket'
       }
     }
-  });
-};
+  })
+}
